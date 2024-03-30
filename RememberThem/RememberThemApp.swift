346861ceaf967a3b1dec6cadb59317e46d6a71b0
enesozmus <ensozmus@gmail.com>
2024-03-30 23:01:50 +0300

    Initial Commit

---
+++ RememberThem/RememberThemApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberThemApp.swift
+//  RememberThem
+//
+//  Created by enesozmus on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct RememberThemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
